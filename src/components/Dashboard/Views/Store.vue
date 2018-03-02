45398ad4424228b9d42ba318e9b143c804bd3542
nourhansamyy <36335033+nourhansamyy@users.noreply.github.com>
2018-03-02 20:02:19 +0200

    Add files via upload

---
+++ src/components/Dashboard/Views/Store.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <paper-table :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns">
+
+          </paper-table>
+        </div>
+      </div>
+
+    </div>
+</template>
+<script>
+  import PaperTable from 'components/UIComponents/PaperTable.vue'
+  const tableColumns = ['Id', 'Name', 'Price', 'CreatedAt', 'UpdatedAt', 'SellerName']
+  const tableData = [{
+    id: 1,
+    name: ' Black Prom Dress',
+    price: '$16.738',
+    createdat: '15/2/2018',
+    updatedat: '16/2/2018',
+    sellername: 'Nourhan Samy'
+  },
+
+  {
+    id: 2,
+    name: 'Clock',
+    price: '$26.738',
+    createdat: '15/2/2018',
+    updatedat: '16/2/2018',
+    sellername: 'Passant Omar'
+  },
+
+  {
+    id: 2,
+    name: 'Sweater',
+    price: '$346.738',
+    createdat: '15/2/2018',
+    updatedat: '16/2/2018',
+    sellername: 'Khaled EL Shimi'
+  },
+
+  {
+    id: 3,
+    name: 'Pantalone',
+    price: '$134.738',
+    createdat: '15/2/2018',
+    updatedat: '16/2/2018',
+    sellername: 'Rawan shehab'
+  },
+  {
+    id: 3,
+    name: 'Dog',
+    price: '$176.738',
+    createdat: '15/2/2018',
+    updatedat: '16/2/2018',
+    sellername: ' Mina Ramsis'
+  }
+]
+
+  export default {
+    components: {
+      PaperTable
+    },
+    data () {
+      return {
+        table1: {
+          title: ' Component 1 Olx',
+          subTitle: 'Here is the products for this table',
+          columns: [...tableColumns],
+          data: [...tableData]
+        },
+        table2: {
+          title: 'Table on Plain Background',
+          subTitle: 'Here is a subtitle for this table',
+          columns: [...tableColumns],
+          data: [...tableData]
+        }
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
---
+++ src/components/Dashboard/Views/UserProfile/Storre.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+        </div>
+      </div>
+      <input type="text" v-model= "product.name"> Name <br>
+      <input type="text" v-model= "product.price"> Price <br>
+        <button @click= "AddToApi"> add product</button> <br>
+        <input type="text" v-model= "product.id"> product id <br>
+          <button @click= "del"> del </button> <br>
+        <table class="table table-striped table-borders">
+        <thead>
+        <tr>
+        <th class="center"> ID </th>
+        <th class="center"> Name </th>
+        <th class="center"> Price </th>
+        <th class="center"> Created at </th>
+        <th class="center"> Updated at </th>
+        <th class="center"> Seller Name </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="prod in prods.data">
+        <td class="test-left"> {{prod._id}}</td>
+        <td class="test-left"> {{prod.name}}</td>
+        <td class="test-left"> {{prod.price}}</td>
+        <td class="test-left"> {{prod.createdAt}}</td>
+        <td class="test-left"> {{prod.updatedAt}}</td>
+        <td class="test-left"> Sama </td>
+        </tr>
+        </tbody>
+        </table>
+    </div>
+</template>
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'Store',
+  data () {
+    return {
+      product: {
+        id: '',
+        name: '',
+        price: '',
+        createdat: '',
+        updatedat: ''
+      },
+      prods: []
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:3000/api/product/getProducts')
+    .then(response => {
+      console.log(response.data)
+      this.prods = response.data
+      console.log(prods)
+    })
+  },
+  methods: {
+    AddToApi () {
+      axios.post('http://localhost:3000/api/product/createProduct', {
+        name: this.product.name,
+        price: this.product.price
+      })
+    .then((response) => {
+      console.log(response)
+      console.log('Hi')
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+      window.location.reload()
+    },
+    del () {
+      axios.delete('http://localhost:3000/api/product/deleteProduct/5a969e6131d0941d3c265eb2')
+      .then((response) => {
+        console.log(response)
+        console.log('Hi')
+      })
+      window.location.reload()
+    }
+  }
+}
+
+</script>
+<style>
+
+</style>
